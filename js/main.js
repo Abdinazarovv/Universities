c3672f33170242626d9163a2135a0e7a8ebbbb61
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-08-15 23:23:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let elWrapper = document.querySelector(".wrapper");
+let elTemplate = document.querySelector("#table__row").content;
+
+function renderUniversities(array) {
+    elWrapper.innerHTML = null
+    let newFragment = document.createDocumentFragment()
+    
+    for (const item of array) {
+        let newRow = elTemplate.cloneNode(true)
+        
+        newRow.querySelector(".uni__name").textContent = item.name;
+        newRow.querySelector(".uni__url").textContent = item.web_pages[0];
+        newRow.querySelector(".uni__url").href = item.web_pages[0];
+        newRow.querySelector(".uni__url").setAttribute("target", "blank")
+        
+        newFragment.appendChild(newRow)
+    }
+    elWrapper.appendChild(newFragment)
+}
+
+
+fetch("http://universities.hipolabs.com/search?country=united+states")
+.then(res => res.json())
+.then(data => renderUniversities(data))
